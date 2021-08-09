3b40b19e3da3053c4b6c54b67e9d4378abeb8dd4
Alexander <agrachev@iflex.ru>
2021-08-09 23:05:34 +0300

    customizable animations on page scroll. 50% desktop done

---
+++ scripts/animations.js
@@ -0,0 +1,35 @@
+// document.querySelector('.animated-header').style.display = 'none';
+
+// const header = anime({
+//     targets: '.animated-header',
+//     keyframes: [
+//         {translateY: '70vh'},
+//         {opacity: 1},
+//         {translateY: 0}
+//     ],
+//     opacity: 1,
+//     duration: 1300,
+//     easing: 'easeOutQuart'
+// });
+
+// const featuresOdd = anime({
+//     targets: '.features-container__feature-block.odd',
+//     // keyframes: [
+//     //     {translateY: -150},
+//     //     {opacity: 1},
+//     //     {translateY: 0}
+//     // ],
+//     translateY: [-345, 0],
+//     duration: 1500,
+//     easing: 'easeOutQuart',
+//     // autoplay: false
+// });
+//
+// const featuresEven = anime({
+//     targets: '.features-container__feature-block.even',
+//     translateY: [345, 0],
+//     duration: 1500,
+//     easing: 'easeOutQuart',
+//     // autoplay: false
+// });
+
