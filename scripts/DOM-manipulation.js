3618ab823e89a200ce5dd10cf9ce1bc2c4799c59
Alexander <agrachev@iflex.ru>
2021-08-31 09:56:33 +0300

    final

--- scripts/DOM-manipulation.js
+++ scripts/DOM-manipulation.js
@@ -1,13 +1,10 @@
+const key_blocks = document.querySelectorAll('.section-key__content ul li');
 
+let BASE_WIDTH = 86;
 
-// async function f2() {
-//     const key_blocks = document.querySelectorAll('.section-key__content ul li');
-//
-//     let BASE_WIDTH = 370;
-//     for (let i = 0; i <= key_blocks.length; i++) {
-//         BASE_WIDTH += 30;
-//         key_blocks[0].style.width = '370px';
-//         key_blocks[i+1].style.width = BASE_WIDTH + 'px';
-//     }
-// }
+for (let i = 0; i <= key_blocks.length; i++) {
+    BASE_WIDTH += 6;
+    key_blocks[0].style.width = '86%';
+    key_blocks[i+1].style.width = BASE_WIDTH + '%';
+}
 
